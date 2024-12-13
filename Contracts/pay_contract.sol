79a5138d49e7bc7fb90334bc0e0c16bfa0a1a729
Fathi123-max <Fathiwehba5@gmail.com>
2024-12-13 22:39:58 +0400

    dollar convert

--- Contracts/pay_contract.sol
+++ Contracts/pay_contract.sol
@@ -1,16 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract PayContract {
-    
+    uint256 public balance;
+    AggregatorV3Interface aggregator =
+        AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
 
+    function getPriceOfEth(uint256 amount) public view returns (uint256) {
+        //conversion rth rate  in 1e18
+        (, int256 answer, , , ) = aggregator.latestRoundData();
+        uint256 conversionRate = uint256(answer * 1e10);
 
-    function Fund () public payable  {
-        
+        //price in dollars
+        //conversion rate in eth is  1e18 / conversionRate
+        uint256 price = (amount * conversionRate) / 1e18;
 
-        require(msg.value > 10 gwei, "Payment must be 10 euros");
-
+        return price;
     }
 
+    function Fund() public payable {
+        balance += msg.value;
 
+        require(getPriceOfEth(msg.value) > 1e18, "Payment must be 1 dollar");
+    }
 }
